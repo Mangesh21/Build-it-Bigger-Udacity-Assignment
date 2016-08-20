438a26e0b98a5f8be751218d6582d776a4fdeabf
Mangesh <mangesh.kadam@citruspay.com>
2016-08-20 23:25:56 +0530

    * separate class for TestCase.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointAsyncTaskTest.java
@@ -1,23 +1,21 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
 import android.text.TextUtils;
+
+import junit.framework.TestCase;
 
 import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by mangesh on 16/8/16.
  */
-public class EndPointAsyncTaskTest extends ApplicationTestCase<Application> {
+public class EndPointAsyncTaskTest extends TestCase implements Callback{
 
     String result = null;
     Exception mError = null;
     CountDownLatch signal = null;
 
-    public EndPointAsyncTaskTest(Class<Application> applicationClass) {
-        super(applicationClass);
-    }
+
 
     @Override
     protected void setUp() throws Exception {
@@ -31,19 +29,17 @@ public class EndPointAsyncTaskTest extends ApplicationTestCase<Application> {
 
 
     public void testjoke() throws InterruptedException {
-
-        EndPointAsyncTask endPointAsyncTask = (EndPointAsyncTask) new EndPointAsyncTask(new Callback() {
-            @Override
-            public void onComplete(String result, Exception e) {
-                EndPointAsyncTaskTest.this.result = result;
-                mError = e;
-                signal.countDown();;
-            }
-        }).execute();
+        EndPointAsyncTask endPointAsyncTask = (EndPointAsyncTask) new EndPointAsyncTask(this).execute();
         signal.await();
-
         assertNull(mError);
         assertFalse(TextUtils.isEmpty(result));
+    }
+
+    @Override
+    public void onComplete(String result, Exception e) {
+        EndPointAsyncTaskTest.this.result = result;
+        mError = e;
+        signal.countDown();
     }
 }
 
